18be02e3f003e20a6083419ccb1603512d6f7d71
D3SP3R4D0S <lee@inha.edu>
2023-09-23 15:44:37 +0900

    [feat] add locals router on app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const booksRouter = require('./routes/books');
+const localsRouter = require('./routes/locals');
 
 const app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/books', booksRouter);
+app.use('/locals', localsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
