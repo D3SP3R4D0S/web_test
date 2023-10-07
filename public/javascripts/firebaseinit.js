7a2ea700b5b57f6028b06adcb1e3582f43ee103d
D3SP3R4D0S <lee@inha.edu>
2023-10-07 15:10:04 +0900

    [feat] apply login and signin through firebase

---
+++ public/javascripts/firebaseinit.js
@@ -0,0 +1,22 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD3BZy3mxI0aXRIACojPHLpwl2cBUXd6r4",
+    authDomain: "fir-53910.firebaseapp.com",
+    projectId: "fir-53910",
+    storageBucket: "fir-53910.appspot.com",
+    messagingSenderId: "509743027710",
+    appId: "1:509743027710:web:304835470a9276f06c1caf",
+    measurementId: "G-3L2K0706QY"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get("/login", (req, res)=>{
+  res.render('index', { title: 'LOGIN', pageName: 'users/login.ejs'});
+})
+
+router.get("/join", (req, res)=>{
+  res.render('index', { title: 'JOIN', pageName: 'users/join.ejs'});
+})
+
 module.exports = router;
