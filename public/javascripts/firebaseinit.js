391eb602478b5d034fde53f12f2f7dcbafa2b16a
D3SP3R4D0S <lee@inha.edu>
2023-10-09 22:39:37 +0900

    [fix] database region fix

--- public/javascripts/firebaseinit.js
+++ public/javascripts/firebaseinit.js
@@ -10,13 +10,13 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.4.0/firebase
 const firebaseConfig = {
     apiKey: "AIzaSyD3BZy3mxI0aXRIACojPHLpwl2cBUXd6r4",
     authDomain: "fir-53910.firebaseapp.com",
+    databaseURL: "https://fir-53910-default-rtdb.asia-southeast1.firebasedatabase.app",
     projectId: "fir-53910",
     storageBucket: "fir-53910.appspot.com",
     messagingSenderId: "509743027710",
     appId: "1:509743027710:web:304835470a9276f06c1caf",
     measurementId: "G-3L2K0706QY"
 };
-
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
