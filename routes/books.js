fcc17432919a932bbcf3a0a2fd2b05367cd707e2
D3SP3R4D0S <lee@inha.edu>
2023-10-14 15:39:04 +0900

    [feat] add book cart

--- routes/books.js
+++ routes/books.js
@@ -2,8 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
+/* 도서검색 */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: '도서검색', pageName:'./books/search.ejs' });
+    res.render('index', { title: '도서검색', pageName:'books/search.ejs' });
+});
+
+//장바구니 페이지
+router.get('/cart', function(req, res){
+    res.render('index', { title: '장바구니', pageName:'books/cart.ejs'});
 });
 
 module.exports = router;
