40f8691e769606330e78e4d0cfd682e8fec0a544
D3SP3R4D0S <lee@inha.edu>
2023-11-04 15:23:00 +0900

    [feat] add firestore mypage

--- routes/users.js
+++ routes/users.js
@@ -14,4 +14,9 @@ router.get("/join", (req, res)=>{
   res.render('index', { title: 'JOIN', pageName: 'users/join.ejs'});
 })
 
+//마이 페이지
+router.get('/mypage', function(req, res){
+  res.render('index', {title:'마이페이지', pageName:'users/mypage.ejs'});
+});
+
 module.exports = router;
