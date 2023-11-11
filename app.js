281bd156dd9868359014e503919ed7065953ca53
D3SP3R4D0S <lee@inha.edu>
2023-11-11 15:26:46 +0900

    [feat] w9 basic board, write

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const booksRouter = require('./routes/books');
 const localsRouter = require('./routes/locals');
+const postssRouter = require('./routes/posts');
 
 const app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/books', booksRouter);
 app.use('/locals', localsRouter);
+app.use('/posts', postssRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
